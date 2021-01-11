9af23c61908b6815dcd1cbc494bbaa8f44cc1ee6
TJ <abongsjoel@gmail.com>
2021-01-11 12:06:02 +0100

    Fetch articles

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Article = () => {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    setTimeout( async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await res.json();
+      setArticles(data);
+    }, 5000);
+  });
+
   return (
     <div className="articles">
       <h2>Artiles</h2>
+
+      {articles && articles.map(article => (
+        <div className="article" key={ article.id }>
+          <h3>{ article.title }</h3>
+          <p>{ article.body }</p>
+        </div>
+      ))}
+
+      {!articles && <div>Loading...</div>}
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -10,6 +10,7 @@ const User = () => {
       setProfile(data);
     }, 5000);
   });
+  
   return (
     <div className="user">
       <h2>User Details</h2>
